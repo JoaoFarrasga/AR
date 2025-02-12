81e520dd99170553241ad2386769b54e3635173d
Joninho Farrasga <joaorantune03@gmail.com>
2025-02-12 18:57:16 +0000

    Last Changes

--- Assets/Things/Scripts/ARObjectClickHandler.cs
+++ Assets/Things/Scripts/ARObjectClickHandler.cs
@@ -4,23 +4,35 @@ public class ARObjectClickHandler : MonoBehaviour
 {
     public GameObject objectToSpawn;
 
-    public Camera arCamera;
-
-    private void Update()
+    void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform == transform)
+                {
+                    Instantiate(objectToSpawn,
+                                hit.transform.position + new Vector3(0, 0.2f, 0),
+                                Quaternion.identity);
+                }
+            }
+        }
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Touch touch = Input.GetTouch(0);
 
-            Ray ray = arCamera.ScreenPointToRay(touch.position);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
             RaycastHit hit;
-
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.transform == transform)
                 {
-                    Instantiate(objectToSpawn, 
+                    Instantiate(objectToSpawn,
                                 hit.transform.position + new Vector3(0, 0.2f, 0),
                                 Quaternion.identity);
                 }
